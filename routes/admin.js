c80e71a88e5275d87e760bc7cdb33ecc2de8f909
rhtjddls123 <jjjk0605@naver.com>
2024-03-16 19:43:38 +0900

    Refactor: admin 경로 필터링

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
 
-router.post("/product", (req, res, next) => {
-  console.log(req.body.title);
+router.post("/add-product", (req, res, next) => {
+  console.log(req.body);
   res.redirect("/");
 });
 
